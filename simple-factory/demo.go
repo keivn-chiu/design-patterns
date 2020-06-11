0962dcfcd0749a3b8871c33b5bf45570335cecd7
Kevin.Chiu <Kevin.Chiu.free@gmail.com>
2020-06-12 07:55:07 +0800

    简单工厂模式

---
+++ simple-factory/demo.go
@@ -0,0 +1,34 @@
+package simple_factory
+
+// 接口
+type domain interface {
+	do()
+}
+
+// 实例 A
+type ADomain struct {
+}
+
+func (*ADomain) do() {
+	// a domain do
+}
+
+// 实例 B
+type BDomain struct {
+}
+
+func (*BDomain) do() {
+	// b domain do
+}
+
+// 工厂方法
+func factoryCreate(name string) domain{
+	switch name {
+	case "a":
+		return &ADomain{}
+	case "b":
+		return &BDomain{}
+	default:
+		return nil
+	}
+}
